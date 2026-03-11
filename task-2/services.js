7988098f3fabc8e1ae4948f68eb8a7062d28adf2
Jawad Al Bdiwi <github.2snsr@passmail.com>
2026-03-11 14:17:39 +0100

    assignment complete

--- task-2/services.js
+++ task-2/services.js
@@ -1,8 +1,8 @@
 // Nobel Prize API Documentation: https://www.nobelprize.org/about/developer-zone-2/
 
-import { fetchData } from './fetcher.js';
+import { fetchData } from "./fetcher.js";
 
-const API_BASE_URL = 'https://api.nobelprize.org/2.1';
+const API_BASE_URL = "https://api.nobelprize.org/2.1";
 
 /**
  * Fetch Nobel Prizes with optional filters
@@ -14,8 +14,24 @@ const API_BASE_URL = 'https://api.nobelprize.org/2.1';
  * @param {Function} onSuccess - Callback for successful fetch
  * @param {Function} onError - Callback for fetch errors
  */
+
 export function fetchNobelPrizes(filters = {}, onSuccess, onError) {
-  let url = ''; // TODO Construct the full URL with query parameters;
+  let url = "";
+  const params = new URLSearchParams();
+
+  params.append("offset", filters.offset);
+  params.append("limit", filters.limit);
+  params.append("sort", "desc");
+
+  if (filters.year !== "all") {
+    params.append("nobelPrizeYear", filters.year);
+  }
+
+  if (filters.category !== "all") {
+    params.append("nobelPrizeCategory", filters.category);
+  }
+
+  url = `${API_BASE_URL}/nobelPrizes?${params.toString()}`;
 
   fetchData(url, onSuccess, onError);
 }
